e40ccde270307493786c72f4e930fcb474223b59
andrers2b <andre.rubin@gmail.com>
2018-01-23 20:27:35 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static_pages/about'
   resources :microposts
   resources :users
-  root 'application#index'
 end
